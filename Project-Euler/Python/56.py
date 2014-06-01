dbe0ce6bc14a181c364c2daa79137cea6eddcf67
Joe Reynolds <joereynolds952@googlemail.com>
2014-06-01 10:40:21 +0100

    Update 56.py

--- Project-Euler/Python/56.py
+++ Project-Euler/Python/56.py
@@ -5,8 +5,6 @@ one followed by two-hundred zeros.
 Despite their size, the sum of the digits in each number is only 1.
 
 Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
-
-
 """
 
 def max_digit_sum():

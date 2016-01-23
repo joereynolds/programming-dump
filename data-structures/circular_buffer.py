32fd50e147b7238b603ef1ec1e792d90add24198
Joe Reynolds <joereynolds952@gmail.com>
2016-01-23 07:44:38 +0000

    Fix bugs with CircularBuffer. Improve tests.

--- data-structures/circular_buffer.py
+++ data-structures/circular_buffer.py
@@ -1,4 +1,19 @@
+"""
+This module contains an implementation of a fixed size Circular Buffer.
+It is created to the specification given on wikipedia. This can be
+seen here: https://en.wikipedia.org/wiki/Circular_buffer
+"""
+
+import unittest
+
 class CircularBuffer():
+    """
+    @BUFFER_START = The start index of the buffer.
+                    Note that this is completely
+                    Arbitrary and can be set to
+                    any value not exceeding
+                    self.size
+    """
 
     BUFFER_START = 2
 
@@ -17,10 +32,17 @@ class CircularBuffer():
     def add_item(self, item):
         """Adds an item to our buffer and takes
         care of all indexing work"""
+        
+        #We need to make sure we check if we're overwriting
+        #a piece of data in the buffer. If we are, set the buffer
+        #start index accordinagly
+        if self.buffer[self.buffer_end] is not None:
+            self.buffer_start += 1
+
         self.buffer[self.buffer_end] = item
         self.buffer_end += 1
 
-        # If the we've reached the end of our buffer,
+        # If we've reached the end of our buffer,
         # reset the 'end' to the beginning
         if self.buffer_end == len(self.buffer):
             self.buffer_end = 0
@@ -35,7 +57,7 @@ class CircularBuffer():
         and takes care of indexing"""
         self.buffer[self.buffer_start] = None
 
-        if self.buffer_start == len(self.buffer) :
+        if self.buffer_start == len(self.buffer) - 1:
             self.buffer_start = 0
         else : self.buffer_start += 1    
 
@@ -51,39 +73,63 @@ class CircularBuffer():
         return str(self.buffer)
 
 
-def show_details(cbuffer):
-    """Shows the start and end points of a buffer.
-    useful for testing only"""
-    print('-'*40)
-    print(cbuffer)
-    print('Start : ',cbuffer.buffer_start)
-    print('End : ',cbuffer.buffer_end)
-
 def test():
     """A test against wikipedia's implementation.
     https://en.wikipedia.org/wiki/Circular_buffer"""
+
+    print('Running tests...')
+    tester = unittest.TestCase()
+
     c = CircularBuffer(7)
-    show_details(c)
+    tester.assertEqual(c.buffer, [None, None, None, None, None, None, None])
+    tester.assertEqual(c.buffer_start, 2)
+    tester.assertEqual(c.buffer_end, 2)
 
     c.add_item(1)
-    show_details(c)
+    tester.assertEqual(c.buffer, [None, None, 1, None, None, None, None])
+    tester.assertEqual(c.buffer_end, 3)
 
     c.add_items(2,3)
-    show_details(c)
+    tester.assertEqual(c.buffer, [None, None, 1, 2, 3, None, None])
 
     c.remove_item()
+    tester.assertEqual(c.buffer, [None, None, None, 2, 3, None, None])
+
     c.remove_item()
-    show_details(c)
+    tester.assertEqual(c.buffer, [None, None, None, None, 3, None, None])
 
     c.add_items(4,5,6,7,8,9)
-    show_details(c)
+    tester.assertEqual(c.buffer, [6, 7, 8, 9, 3, 4, 5])
 
     c.add_items('A','B')
-    show_details(c)
+    tester.assertEqual(c.buffer, [6, 7, 8, 9, 'A', 'B', 5])
 
     c.remove_item()
+    tester.assertEqual(c.buffer, [6, 7, 8, 9, 'A', 'B', None])
+
     c.remove_item()
-    show_details(c)
+    tester.assertEqual(c.buffer, [None, 7, 8, 9, 'A', 'B', None])
+
+#currently unused. Switch to this when possible
+class TestCircularBuffer(unittest.TestCase):
+
+    def __init__(self):
+        self.cBuffer = CircularBuffer(7)
+
+    def test_buffer_start(self):
+        pass
+
+    def test_add_item(self):
+        pass
+
+    def test_add_items(self):
+        pass
+
+    def test_remove_item(self):
+        pass
+
+    def test_destroy(self):
+        pass
 
 if __name__ == '__main__':
     test()    

a1a25af87e64a032f39eec3638d1369f6f1ab74a
Joe Reynolds <joereynolds952@gmail.com>
2016-02-10 20:20:43 +0000

    euler 1

---
+++ challenges/project-euler/c/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(int argc, char** argv)
+{
+    int sum = 0;
+    int i;
+    for (i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0 ) {
+            sum += i;
+            printf("%d \n", i);
+        }
+    }
+    printf("Answer is %d", sum);
+}
+
+
+

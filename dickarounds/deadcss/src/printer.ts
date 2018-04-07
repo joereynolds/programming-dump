604dd6947a4483001bb3e13874d2b9d552ba86db
Joe Reynolds <joereynolds952@gmail.com>
2018-04-07 23:25:55 +0100

    add more sample css files, add progress bar

--- dickarounds/deadcss/src/printer.ts
+++ dickarounds/deadcss/src/printer.ts
@@ -1,4 +1,5 @@
 const chalk = require("chalk");
+const progressBar = require("progress");
 
 class Printer {
 
@@ -6,7 +7,10 @@ class Printer {
      * Prints out the usages for each selector if they are <= userDefinedUsageCount
      */
     public printUsages(selectors: any[], userDefinedUsageCount: number) {
+        // const bar = new progressBar(":bar", { total: selectors.length });
+
         selectors.forEach(selector => {
+            // bar.tick();
             if (selector.usages <= userDefinedUsageCount) {
                 console.log(
                     `${selector.usages} usages found, ${chalk.green(selector.selector)} can probably be removed.`,

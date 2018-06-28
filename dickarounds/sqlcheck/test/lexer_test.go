0dd9fcadf08be8f5ad3bbacaa26c5e0dcb08143a
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2018-06-28 16:53:52 +0100

    sql

---
+++ dickarounds/sqlcheck/test/lexer_test.go
@@ -0,0 +1,13 @@
+package lexer
+
+import (
+    "sqlcheck/lexer"
+    "testing"
+)
+
+func TestLexer(t *testing.T) {
+    if 1 != 2 {
+        t.Errorf("1 does not equal 2")
+    }
+    // lexer.Tokenize()
+}
